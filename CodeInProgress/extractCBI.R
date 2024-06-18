37f1c5ea810fd80717ce10fb64a37118ad3589b6
Andrew Latimer <amlatimer@ucdavis.edu>
2024-06-18 15:51:41 -0700

    update script to also load and extract dnbr from mtbs rasters (geotifs downloaded from MTBS)

--- CodeInProgress/extractCBI.R
+++ CodeInProgress/extractCBI.R
@@ -6,7 +6,7 @@ library(tidyverse)
 library(terra)
 
 
-# Load fire severity rasters 
+# Load fire severity rasters from Mike Koontz (metric is predicted CBI)
 rim_sev <- rast("./Data/Rim_2013-CASTF-002857_CBI_bc_v1.0_2013_extended.tif")
 st_crs(rim_sev)
 ferguson_sev <- rast("./Data/FERGUSON_2018-CASNF-000745_CBI_bc_v1.0_2018_extended.tif")
@@ -14,7 +14,19 @@ plot(ferguson_sev)
 
 # merge fire rasters 
 fire_sev <- merge(rim_sev, ferguson_sev)
+plot(fire_sev) 
+
+# Load fire severity rasters from MTBS (metric is dnbr)
+rim_mtbs <- rast("./Data/ca3765211988120180713_20180705_20190708_dnbr.tif")
+ferguson_mtbs <- rast("./Data/ca3785712008620130817_20130714_20140701_dnbr.tif")
+fire_sev_mtbs <- merge(rim_mtbs, ferguson_mtbs)
+fire_sev_mtbs[fire_sev_mtbs < 0] <- NA
+plot(fire_sev_mtbs)
+
+# reproject Mike's fire raster into MTBS raster projection 
+fire_sev <- project(fire_sev, fire_sev_mtbs) 
 plot(fire_sev)
+plot(fire_sev_mtbs, add=T)
 
 # Load the point data
 yose_pts <- read.csv("./Data/Plot_beg_UTMs_66sr22.csv", header = TRUE) 
@@ -25,21 +37,27 @@ yose_pts_10 <- dplyr::filter(yose_pts, utm_zone == 10) |>
   st_as_sf(coords = c("plot_beg_UTM_E", "plot_beg_UTM_N"), crs = 26910)
 
 # reproject points to raster crs
-yose_pts_11 <- st_transform(yose_pts_11, crs = crs(rim_sev))
-yose_pts_10 <- st_transform(yose_pts_10, crs = crs(rim_sev))
+yose_pts_11 <- st_transform(yose_pts_11, crs = crs(rim_mtbs))
+yose_pts_10 <- st_transform(yose_pts_10, crs = crs(rim_mtbs))
 yose_pts_sf <- rbind(yose_pts_11, yose_pts_10)
 
 # Plot the data to check 
-plot(fire_sev)
+plot(fire_sev_mtbs)
 points(yose_pts_sf, pch = 19, col = "red")
+text(yose_pts_sf, labels = yose_pts$plot_ID, cex = 0.5, pos = 3)
 
-# Extract the values from the raster
+# Extract the values from the CBI raster
 pred_CBI <- extract(fire_sev, yose_pts_sf, method = "bilinear")
-plot_CBI_data <- cbind(yose_pts, pred_CBI)
-names(plot_CBI_data)[7] <- "predicted_CBI"
-plot_CBI_data <- select(plot_CBI_data, -ID)
-head(plot_CBI_data)
-hist(plot_CBI_data$predicted_CBI)
+dnbr <- extract(fire_sev_mtbs, yose_pts_sf, method = "bilinear")
+plot_sev_data <- cbind(yose_pts, pred_CBI, dnbr)
+names(plot_sev_data)[7] <- "predicted_CBI"
+names(plot_sev_data)[9] <- "dnbr"
+head(plot_sev_data)
+hist(plot_sev_data$predicted_CBI)
+hist(plot_sev_data$dnbr)
+plot(predicted_CBI ~ dnbr, data = plot_sev_data) 
+# Pretty good correspondence, except for 5 points where Mike's CBI is much higher than MTBS dnbr
+
 
 # Save the data
 write.csv(plot_CBI_data, "./Data/plot_CBI_data.csv", row.names = FALSE)
