d959a634a5bad67ba2f434cf641497b5066aa894
TazlinaD <tazlinad@gmail.com>
2025-05-04 10:34:59 -0700

    Initial commit

---
+++ Scripts/UnderstoryAnalysis/PercentCover.R
@@ -0,0 +1,17 @@
+# Percent Cover
+# 04 May 2025
+# Author: Tazlina Dentinger
+
+# Input: Cleaned Understory Lists
+
+# Code Description: Calculates percent cover of each species for each plot
+    # Finds percent cover based on both the "raindrop method," where only the highest hit below 2m is counted, and by counting each species hit at each point.
+
+# Output: Percent cover for each species at each plot based on both methods
+
+#Part 1: Read in cleaned understory data ------------------------------------------------------------
+
+#Part 2: Raindrop method ----------------------------------------------------------------------------
+
+#Find the first hit of each species
+#Check that these remain in order
