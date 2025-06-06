4691d792260da11a88f32cc8a1dd084770d24975
TazlinaD <tazlinad@gmail.com>
2025-06-06 12:53:08 -0700

    Reorganize manual data cleaning statements, fix filepaths to match new GitHub organization

--- Scripts/UnderstoryCleaning/3_UnderstoryRecodeCleaning.R
+++ Scripts/UnderstoryCleaning/3_UnderstoryRecodeCleaning.R
@@ -36,7 +36,7 @@ all_plots_understory <- all_plots_understory %>% mutate(hitNum = case_when(
 #Still needs full check with full Unknown Veg Notes
 
 #read in Unknown Veg csv file
-unkVeg <- read_csv("Data/RawData/Test Unknown Veg Notes - UnknownPlants.csv")
+unkVeg <- read_csv("Data/RawData/Unknown Veg Notes - UnknownPlants.csv")
 names(unkVeg) <- gsub(" ", "_", names(unkVeg))
 #keep only relevant columns and YOSE rows
 unkVeg <- unkVeg %>% select(project, asEntered, code, bestGuess, nextSteps, plotID) %>% filter(project == "YOSE")
@@ -47,17 +47,15 @@ unkVeg <- unkVeg %>% mutate(
 all_plots_understory <- all_plots_understory %>% mutate(
   uniqueID = paste0(species, plotID))
 
-#use merge to map codes in dictionary to codes in df - Something's off here
-all_plots_wUnk <- merge(all_plots_understory, unkVeg, by.x = "uniqueID", by.y = "uniqueID" all.x = F) #CHANGE BACK TO TRUE 
-all_plots_merged <- all_plots_wUnk %>% 
+#use merge to map codes in dictionary to codes in df
+all_plots_understory <- merge(all_plots_understory, unkVeg, by.x = "uniqueID", by.y = "uniqueID", all.x = T) #CHANGE BACK TO TRUE 
+all_plots_understory <- all_plots_understory %>% 
   mutate(species = case_when(
     nextSteps %in% c("ID'ed", "Not ID-able") ~ bestGuess, #need to clean up bestGuess column before using - or use Code?
-    asEntered == NA_character_ ~ "ooooooo", #what the hell man - doesn't remotely work. Neither does regular NA or is.na
+    #asEntered == "NA" ~ "ooooooo", #what the hell man - doesn't remotely work. Neither does regular NA or is.na or == "NA"
     #when there is no entry in unkVeg ~ species
     TRUE ~ species
-  )) %>% select(plotID.x, dOut_m, pin_vs_assoc, species, nextSteps) %>% rename(plotID = plotID.x)
-
-#change df name back to whatever feeds into Part 2
+  )) %>% select(plotID.x, dOut_m, pin_vs_assoc, species, hitNum) %>% rename(plotID = plotID.x)
 
 ##-------------------------- Part 2: clean data "manually" in R ----------------------------
 #Includes fixing misspellings and incorrect entries or common names-------------------------
@@ -66,19 +64,19 @@ all_plots_merged <- all_plots_wUnk %>%
 unique(all_plots_understory$species) # 375 (reduced from 501 when there was incorrect incrementing)
 
 # bar chart to look at frequency of unique hits
-all_plots_understory %>% 
-  group_by(species) %>%
-  summarize (n = n()) %>%
-  mutate(total = sum(n),
-         freq = n / total) %>%
-  ggplot() +
-  geom_bar(mapping=aes(x=species, y=freq),stat="identity") +
-  xlab("Hit") +
-  ylab("Proportion") +
-  coord_flip()
+# all_plots_understory %>% 
+#   group_by(species) %>%
+#   summarize (n = n()) %>%
+#   mutate(total = sum(n),
+#          freq = n / total) %>%
+#   ggplot() +
+#   geom_bar(mapping=aes(x=species, y=freq),stat="identity") +
+#   xlab("Hit") +
+#   ylab("Proportion") +
+#   coord_flip()
 
 #Standardize species ending (currently no "sp", no _1 etc.)
-all_plots_understory_trim <- all_plots_understory %>% 
+all_plots_understory <- all_plots_understory %>% 
   mutate(species = case_when(
     str_detect(species, " sp.") ~ unlist(str_split(species, " "))[1],
     str_detect(species, "Taraxacum sp") ~ "Taraxacum", #only case that doesn't include a "."
@@ -87,7 +85,7 @@ all_plots_understory_trim <- all_plots_understory %>%
     TRUE ~ species
   ))
 
-# Correct misspellings and misidentifications
+# Correct misspellings 
 all_plots_understory <- all_plots_understory %>% 
   mutate(species = case_when(
     species %in% c("littter", "ltter", "littler", "litterlitter", "lItter", "pinecone", "bark") ~ "litter", #removed "unknown_DD" from list - should be in wood
@@ -95,38 +93,67 @@ all_plots_understory <- all_plots_understory %>%
     species %in% c("rcok", "gravel") ~ "rock",
     str_detect(species, "_DD") ~ "wood",
     species == "Pranus" ~ "Prunus",
-    species == "VECA" ~ "Fragaria vesca",
     species %in% c("PPREM", "PREM") ~ "Prunus emarginata",
     species == "cynocerous" ~ "Cynosurus echinatus",
     species %in% c("striptanthis", "streptanthus tortuosus") ~ "Streptanthus tortuosus",
     species %in% c("Saxifrogacea", "saxifrogacea", "Saxifrage", "unk_saxifrage") ~ "Saxifragaceae",
     species %in% c("castilleja", "castelleja") ~ "Castilleja",
     species == "enicamena" ~ "Ericameria",
-    species == "Eriogonum sp." ~ "Eriogonum" ,
     species == "Eriogunum sp." ~ "Eriogonum",
     species == "Senicio glomerata" ~ "Senecio glomerata",
     species == "Achillea millefolia" ~ "Achillea millifolium",
-    species == "AZOC" ~ "Rhododendron occidentale",
     species == "CAREXI" ~ "Carex",
     species == "Pseudoghaphalium californicum" ~ "Pseudognaphalium californicum",
+    species %in% c("unk_apeaceae", "unk_apiaceae") ~ "Apiaceae",
     species == "HIAE" ~ "HIAL",
     species == "CIEN" ~ "CEIN",
     species == "GRER" ~ "GAER",
     species == "COMO" ~ "CAMO",
     species == "STFO" ~ "STTO",
-    species %in% c("SYCO", "GAHI") ~ "SYMO", #paper data sheet said creeping snowberry; recoded to correct snowberry
+    species == "SYCO" ~ "SYMO",
     species == "PITO" ~ "Diplacus torreyi",
     species == "ERHU" ~ "ERNU",
     species == "DOOB" ~ "GOOB",
-    species == "MYOD" ~ "Osmorhiza", #paper data sheet said sweet cicely; recoded to correct sweet cicely
     species == "GADS" ~ "GADI",
     species == "RALE" ~ "RULE",
-    species == "QUDU" ~ "Quercus berberidifolia", #incorrect scrub oak
     species == "DEMI" ~ "DERI", #misspelling of species code
     species == "PUCA" ~ "ROCA",
+    grepl("^QUWE_", species, ignore.case = TRUE) ~ "QUWI",
+    species == "Erythranthre" ~ "Erythranthe",
+    species == "RINI" ~ "RIVI", #incorrectly entered from plot 19
+    species == "ARVE" ~ "ARVI", #over walkie-talkie...
+    TRUE ~ species
+  ))
+
+# Convert common names to scientific and fix scientific names
+all_plots_understory <- all_plots_understory %>% 
+  mutate(species = case_when(
+    #species %in% c("moss1", "moss2") ~ "moss", # do we want to count brophytes, lichens, etc???
+    species == "cryptantha/plagiobothrys" ~ "Boraginaceae",
+    species == "brome" ~ "Bromus",
+    species == "fireweed" ~ "CHAN",
+    species == "horsetail" ~ "Equisetum",
+    species == "rush" ~ "Juncus",
+    species == "elgl" ~ "ELGL",
+    species %in% c("whisker plant", "whisker brush", "whiskerbrush") ~ "LECI",
+    species == "goldenrod" ~ "Solidago",
+    species == "vetch" ~ "Vicia",
+    species == "fern" ~ "Polypodiopsida",
+    species == "Dodder" ~ "Cuscuta",
+    species == "aster" ~ "Asteraceae",
+    species == "grass" ~ "Poaceae",
+    TRUE ~ species
+  ))
+
+# Correct misidentifications and incorrect entries
+all_plots_understory <- all_plots_understory %>% 
+  mutate(species = case_when(
+    species == "AZOC" ~ "Rhododendron occidentale",
+    species == "GAHI" ~ "SYMO", #paper data sheet said creeping snowberry; recoded to correct snowberry
+    species == "MYOD" ~ "Osmorhiza", #paper data sheet said sweet cicely; recoded to correct sweet cicely
+    species == "QUDU" ~ "Quercus berberidifolia", #incorrect scrub oak
     species == "SASC" ~ "Salix", #insufficient evidence of species-level ID
     species == "PEHE" & plotID %in% c("3", "4", "9") ~ "Penstemon", #paper data sheet said penstemon or blue penstemon
-    grepl("^QUWE_", species, ignore.case = TRUE) ~ "QUWI",
     species == "TOPU" ~ "TODI", #incorrect poison oak entered
     species == "CAUM" ~ "Calyptridium", #paper said pussypaws
     species == "PENE" ~ "Penstemon", #paper said maybe newberrii
@@ -137,53 +164,18 @@ all_plots_understory <- all_plots_understory %>%
     species == "LOIN" ~ "shrub_1", #based on note on datasheet 
     species == "LUMA" ~ "LULA", #L. macrophyllum doesn't exist
     species == "Juncus patens" ~ "Juncus", #insufficient evidence of species-level ID
-    species == "Erythranthre" ~ "Erythranthe",
-    species == "RINI" ~ "RIVI", #incorrectly entered from plot 19
     species == "epilobium" & plotID == 25 ~ "Epilobium brachycarpum",
+    species == "PESP" & plotID == 2 ~ "Penstemon", 
+    species == "Draperia drymariodes" ~ "Draperia systyla", #incorrect specific epithet written down
+    species == "PSMI" ~ "Pseudognaphalium", #pseudognaphaliums too difficult to identify
+    species == "lessingia" & (plotID == 29 | plotID == 34) ~ "Lessingia leptoclada",
+    species == "LUAR" ~ "Lupinus", #unresolved duplicate code
+    species == "GATR" & (plotID %in% c(35, 45, 54, 12, 32, 59, 49)) ~ "Galium triflorum",
+    species == "FRCA" ~ "Frangula", 
+    species %in% c("TAER", "TAOC") ~ "Taraxacum",
+    species == "CYOF" & plotID == 21 ~ "Adelinia grandis",
     TRUE ~ species
   ))
-
-# Look at all unique hits
-unique(all_plots_understory$species) #was 483, down to 339 by removing incrementing, recoding some incorrect entries
- 
-# Convert common names to scientific and fix scientific names
-all_plots_understory <- all_plots_understory %>% 
-  mutate(species = case_when(
-    #species %in% c("moss1", "moss2") ~ "moss", # do we want to count brophytes, lichens, etc???
-    species == "cryptantha/plagiobothrys" ~ "Boraginaceae",
-    species == "brome" ~ "Bromus",
-    species == "fireweed" ~ "CHAN",
-    species == "horsetail" ~ "Equisetum",
-    species == "rush" ~ "juncus",
-    species %in% c("unk_apeaceae", "unk_apiaceae") ~ "Apiaceae",
-    species == "elgl" ~ "ELGL",
-    species %in% c("whisker plant", "whisker brush", "whiskerbrush") ~ "LECI",
-    species == "Taraxacum sp" ~ "Taraxacum",
-    species == "goldenrod" ~ "Solidago",
-    species == "vetch" ~ "Vicia",
-    species == "fern" ~ "Polypodiopsida",
-    species == "Dodder" ~ "Cuscuta",
-    species == "aster" ~ "Asteraceae",
-    species == "grass" ~ "Poaceae",
-    species == "piperia" ~ "Platanthera", #genus Pipera subsumed
-    species == "SACE" ~ "SAME", #Blue Elder moved to S. mexicana
-    species == "CYOF" ~ "Adelinia grandis",
-    
-    TRUE ~ species
-  ))
-
-# # convert codes to scientific names - will be taken care of with dictionary
-# known <- known %>% mutate(species == case_when(
-#   species == "BRTE" ~ "Bromus tectorum",
-#   species == "RIRO" ~ "Ribes roezlii",
-#   species == "CEIN" ~ "Ceanothus integerrimus",
-#   species == "CHFO" ~ "Chamaebatia foliosa",
-#   species == "ARVI" ~ "Arctostaphylos viscida"
-#   )
-# )
-
-# Look at all unique hits
-unique(all_plots_understory$species) # 479 ->333
 
 ##----------- Part 3: lump various Poales, forbs, life stages for visualization ---------------------------
 
@@ -194,17 +186,13 @@ carexcheck <- all_plots_understory %>%
     str_detect(species, "(?i)carex sp") ~ "Carex",
     TRUE ~ species
   ))
-# Look at all unique hits
-unique(carexcheck$species) # 424 ->328
 
 # lump all unknown grasses for now
 grassgroup <- carexcheck %>% 
   mutate(species = case_when(
-    grepl("^grass_", species, ignore.case = TRUE) ~ "Poaceae", #any common names w/ grass that aren't Poaceae?
+    grepl("^grass_", species, ignore.case = TRUE) ~ "Poaceae", 
     TRUE ~ species
   ))
-# Look at all unique hits
-unique(grassgroup$species) # 373 ->236
 
 # lump all unknown forbs for now
 forbfest <- grassgroup %>% 
@@ -214,104 +202,119 @@ forbfest <- grassgroup %>%
     TRUE ~ species
   ))
 
-# Look at all unique hits
-unique(forbfest$species) # 315 -> 318
-
-# bar chart to look at frequency of unique hits
-forbfest %>% 
-  group_by(species) %>%
-  summarize (n = n()) %>%
-  mutate(total = sum(n),
-         freq = n / total) %>%
-  ggplot() +
-  geom_bar(mapping=aes(x=species, y=freq),stat="identity") +
-  xlab("Hit") +
-  ylab("Proportion") +
-  theme(axis.text.x=element_text(angle=90,hjust=1))
-
-# Consolidate life stages--actually maybe just for richness not for percent cover 
-allLifeStages <- forbfest %>% 
-  mutate(species = case_when(
-    grepl("^ABCO_", species, ignore.case = TRUE) ~ "ABCO",
-    grepl("^ABMA_", species, ignore.case = TRUE) ~ "ABMA",
-    grepl("^ACMA_", species, ignore.case = TRUE) ~ "ACMA",
-    grepl("^CADE_", species, ignore.case = TRUE) ~ "Calocedrus decurrens",
-    grepl("^JUOC_", species, ignore.case = TRUE) ~ "JUOC",
-    grepl("^PILA_", species, ignore.case = TRUE) ~ "PILA",
-    grepl("^PIJE_", species, ignore.case = TRUE) ~ "PIJE",
-    grepl("^PIPO_", species, ignore.case = TRUE) ~ "Pinus ponderosae",
-    grepl("^PSME_", species, ignore.case = TRUE) ~ "PSME",
-    grepl("^QUCH_", species, ignore.case = TRUE) ~ "QUCH",
-    grepl("^QUKE_", species, ignore.case = TRUE) ~ "QUKE",
-    grepl("^QUWI_", species, ignore.case = TRUE) ~ "QUWI",
-    TRUE ~ species
-  ))
-
-unique(allLifeStages$species) # 283 ->292
-
-# bar chart to look at frequency of unique hits
-allLifeStages %>% 
-  group_by(species) %>%
-  summarize (n = n()) %>%
-  mutate(total = sum(n),
-         freq = n / total) %>%
-  ggplot() +
-  geom_bar(mapping=aes(x=species, y=freq),stat="identity") +
-  xlab("Hit") +
-  ylab("Proportion") +
-  theme(axis.text.x=element_text(angle=90,hjust=1))
-
-# group by plot 
-allLifeStages %>% group_by(plotID)  %>% 
-summarize (n = n()) %>%
-  mutate(total = sum(n),
-         freq = n / total) %>%
-  ggplot() +
-  geom_bar(mapping=aes(x=plotID, y=freq),stat="identity") +
-  xlab("Hit") +
-  ylab("Proportion") +
-  theme(axis.text.x=element_text(angle=90,hjust=1))
+all_plots_understory <- forbfest
+# # Look at all unique hits
+# unique(forbfest$species) # 315 -> 318
+# 
+# # bar chart to look at frequency of unique hits
+# forbfest %>% 
+#   group_by(species) %>%
+#   summarize (n = n()) %>%
+#   mutate(total = sum(n),
+#          freq = n / total) %>%
+#   ggplot() +
+#   geom_bar(mapping=aes(x=species, y=freq),stat="identity") +
+#   xlab("Hit") +
+#   ylab("Proportion") +
+#   theme(axis.text.x=element_text(angle=90,hjust=1))
+# 
+# # Consolidate life stages--actually maybe just for richness not for percent cover 
+# allLifeStages <- forbfest %>% 
+#   mutate(species = case_when(
+#     grepl("^ABCO_", species, ignore.case = TRUE) ~ "ABCO",
+#     grepl("^ABMA_", species, ignore.case = TRUE) ~ "ABMA",
+#     grepl("^ACMA_", species, ignore.case = TRUE) ~ "ACMA",
+#     grepl("^CADE_", species, ignore.case = TRUE) ~ "Calocedrus decurrens",
+#     grepl("^JUOC_", species, ignore.case = TRUE) ~ "JUOC",
+#     grepl("^PILA_", species, ignore.case = TRUE) ~ "PILA",
+#     grepl("^PIJE_", species, ignore.case = TRUE) ~ "PIJE",
+#     grepl("^PIPO_", species, ignore.case = TRUE) ~ "Pinus ponderosae",
+#     grepl("^PSME_", species, ignore.case = TRUE) ~ "PSME",
+#     grepl("^QUCH_", species, ignore.case = TRUE) ~ "QUCH",
+#     grepl("^QUKE_", species, ignore.case = TRUE) ~ "QUKE",
+#     grepl("^QUWI_", species, ignore.case = TRUE) ~ "QUWI",
+#     TRUE ~ species
+#   ))
+# 
+# unique(allLifeStages$species) # 283 ->292
+# 
+# # bar chart to look at frequency of unique hits
+# allLifeStages %>% 
+#   group_by(species) %>%
+#   summarize (n = n()) %>%
+#   mutate(total = sum(n),
+#          freq = n / total) %>%
+#   ggplot() +
+#   geom_bar(mapping=aes(x=species, y=freq),stat="identity") +
+#   xlab("Hit") +
+#   ylab("Proportion") +
+#   theme(axis.text.x=element_text(angle=90,hjust=1))
+# 
+# # group by plot 
+# allLifeStages %>% group_by(plotID)  %>% 
+# summarize (n = n()) %>%
+#   mutate(total = sum(n),
+#          freq = n / total) %>%
+#   ggplot() +
+#   geom_bar(mapping=aes(x=plotID, y=freq),stat="identity") +
+#   xlab("Hit") +
+#   ylab("Proportion") +
+#   theme(axis.text.x=element_text(angle=90,hjust=1))
 
 ##-------- Part 4: Use Species Code Dictionary & Taxonstand to apply scientific names --------
-#This may eventually migrate to a separate script
 
 #read in Species Code Dictionary csv file
-spDict <- read_csv("Data/RawData/Test Species Code Dictionary - Sheet1.csv")
+spDict <- read_csv("dataSandbox/Dictionnaries/Species Code Dictionary.csv")
 #replace spaces with underscores in column names
 names(spDict) <- gsub(" ", "_", names(spDict))
 #keep only relevant columns
 spDict <- spDict %>% select(Code, Scientific_Name)
 
 #use merge to map codes in dictionary to codes in df
-all_plots_understory <- merge(all_plots_understory, spDict, by.x = "species", by.y = "Code", all.x = F) #CHANGE BACK TO TRUE to keep all vals
-all_plots_understory <- all_plots_understory %>% 
+all_plots_understory_test <- merge(all_plots_understory, spDict, by.x = "species", by.y = "Code", all.x = T) #CHANGE BACK TO TRUE to keep all vals
+all_plots_understory_test <- all_plots_understory_test %>% 
   mutate(species = case_when(
-    !is.na(all_plots_understory$Scientific_Name) ~ all_plots_understory$Scientific_Name
+    !is.na(all_plots_understory_test$Scientific_Name) ~ all_plots_understory_test$Scientific_Name, #is.na doesn't work still - converts a bunch of data to na
+    TRUE ~ species
   )) %>% select(!Scientific_Name)
 
-##########################################################################################
-#From here on, all of this will apply to both YOSE and SEKI data. May need to move to another script or bring in SEKI data
+#check species names
+unique(all_plots_understory$species)
 
+#Taxonstand
 library(U.Taxonstand)
-library(readxl)
 
 #U.Taxonstand database
-spDatabase1 <- read_excel("C:/Users/tazli/Downloads/YOSE_SugarPine/DataClean/Plants_LCVP_database_part1.xlsx")
-spDatabase2 <- read_excel("C:/Users/tazli/Downloads/YOSE_SugarPine/DataClean/Plants_LCVP_database_part2.xlsx")
-spDatabase3 <- read_excel("C:/Users/tazli/Downloads/YOSE_SugarPine/DataClean/Plants_LCVP_database_part3.xlsx")
+spDatabase1 <- read_csv("C:/Users/tazli/Downloads/YOSE_SugarPine/MultipleDisturbances/dataSandbox/Dictionnaries/Plants_LCVP_database_part1.csv")
+spDatabase2 <- read_csv("C:/Users/tazli/Downloads/YOSE_SugarPine/MultipleDisturbances/dataSandbox/Dictionnaries/Plants_LCVP_database_part2.csv")
+spDatabase3 <- read_csv("C:/Users/tazli/Downloads/YOSE_SugarPine/MultipleDisturbances/dataSandbox/Dictionnaries/Plants_LCVP_database_part3.csv")
 spDatabase <- rbind(spDatabase1, spDatabase2, spDatabase3)
 rm(spDatabase1, spDatabase2, spDatabase3)
 
 #use U.Taxonstand to spell check scientific names (takes a second)
-nameMatch <- nameMatch(spList=known$species, spSource=spDatabase, author = TRUE, max.distance= 4)
+nameMatch <- nameMatch(spList=all_plots_understory$species, spSource=spDatabase, author = TRUE, max.distance= 4)
 #keep only rows with fuzzy matching
-nameMatchFuzzy <- nameMatch %>% select(Submitted_Name, Fuzzy, Name_in_database) %>% filter(Fuzzy == TRUE) %>% unique()
+nameMatchFuzzy <- nameMatch %>% select(Submitted_Name, Fuzzy, Name_in_database) %>% filter(Fuzzy == TRUE) #%>% unique()
 #match accepted names to the misspelled rows
-known <- merge(known, nameMatchFuzzy, by.x = "species", by.y = "Submitted_Name", all.x = TRUE)
+all_plots_understory <- merge(all_plots_understory, nameMatchFuzzy, by.x = "species", by.y = "Submitted_Name", all.x = TRUE)
 #change misspelled names to database names
-known <- known %>% mutate(species = case_when(
+all_plots_understory <- all_plots_understory %>% mutate(species = case_when(
   !is.na(Name_in_database) & Fuzzy == TRUE ~ Name_in_database, 
   TRUE ~ species
 )) %>% select(species, plotID, dOut_m, pin_vs_assoc)
+
+#Address name changes: What standard to use? 
+all_plots_understory <- all_plots_understory %>% 
+  mutate(species = case_when(
+    species == "piperia" ~ "Platanthera", #genus Pipera subsumed
+    species == "SACE" ~ "SAME", #Blue Elder moved to S. mexicana
+    TRUE ~ species
+  ))
+
+##-------------------------------Add data quality flags----------------------------
+#Sanicula crassicaulis YPE 32 and 50 confident to genus
+#Galium triflorum YPE 12, 59, 32 confident to genus
+#Galium porrigens YPE 73 confident to genus
+#Agrostis ype 9 confident to family
 
 ##-------------------------------Part 6: Write dataframe to csv-----------------------------------
